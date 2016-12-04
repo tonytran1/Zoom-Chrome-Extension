47c25eaa4525f7d8a0f21b2e5dea72bb18e47604
tonytran1 <tonylyfe@gmail.com>
2016-12-03 20:19:54 -0800

    Added function to hold down scaling buttons

--- js/zoom.js
+++ js/zoom.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  var scaleUpInterval;
+  var scaleDownInterval;
   var scale = 1.00;
 
   $('#zoom-in').on('click', function() {
@@ -9,6 +11,24 @@ $(document).ready(function () {
   $('#zoom-out').on('click', function() {
     scale -= 0.005;
     sendToTab(scale);
+  });
+
+  $('#zoom-in').on('mousedown', function() {
+    scaleUpInterval = setInterval(function() {
+      scale += 0.005;
+      sendToTab(scale);
+    }, 20);
+  }).on('mouseup mouseleave', function() {
+    clearInterval(scaleUpInterval);
+  });
+
+  $('#zoom-out').on('mousedown', function() {
+    scaleDownInterval = setInterval(function() {
+      scale -= 0.005;
+      sendToTab(scale);
+    }, 20);
+  }).on('mouseup mouseleave', function() {
+    clearInterval(scaleDownInterval);
   });
 
   $('#reset').on('click', function() {
