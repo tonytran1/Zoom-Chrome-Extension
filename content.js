2a945f3985c5baf7e8d37f24ad9bf4712f2a5448
tonytran1 <tonylyfe@gmail.com>
2016-12-03 17:38:15 -0800

    Zooming working

--- content.js
+++ content.js
@@ -1,11 +1,14 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.scale) {
-    setScale(request.scale);
-  }
-});
+(function() {
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.scale) {
+      setScale(request.scale);
+    }
+    return true;
+  });
 
-function setScale(scale) {
-  document.body.style.webkitTransform =  'scale(' + scale +')';
-  document.body.style.msTransform =   'scale(' + scale +')';
-  document.body.style.transform = 'scale(' + scale +')';
-}
+  function setScale(scale) {
+    document.body.style.webkitTransform =  'scale(' + scale +')';
+    document.body.style.msTransform =   'scale(' + scale +')';
+    document.body.style.transform = 'scale(' + scale +')';
+  }
+})();
--- zoom.js
+++ zoom.js
@@ -1,18 +1,19 @@
-var scale = 1.0;
+$(document).ready(function () {
+  var scale = 1.0;
 
-$('#zoom-in').on('click', function() {
-  scale += 0.1;
-  sendToTab(scale);
-});
-
-$('#zoom-out').on('click', function() {
-  scale -= 0.1;
-  sendToTab(scale);
-});
-
-
-function sendToTab(scale) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { scale: scale });
+  $('#zoom-in').on('click', function() {
+    scale += 0.01;
+    sendToTab(scale);
   });
-}
+
+  $('#zoom-out').on('click', function() {
+    scale -= 0.01;
+    sendToTab(scale);
+  });
+
+  function sendToTab(scale) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { scale: scale });
+    });
+  }
+});
